889196347be78265b4ae99033fd86f6dd386c827
Gabrielle Salamanca <knightcallie73@hotmail.com>
2023-06-29 14:31:00 -0700

    Merged healthy & covid into combine dataset, replaced NAs with 1e-7, dim check

--- Code Script/Gene Testing V2.R
+++ Code Script/Gene Testing V2.R
@@ -49,3 +49,19 @@ summary(patients)
 describe(C19vj)
 describe(vj)
 describe(patients)
+
+#############
+# Data Prep #
+#############
+
+# combine the vj datasets
+combine <- merge(C19vj, vj, by = "vjGene")
+
+# replace NAs w/ extremely small number (10e-8)
+combine[is.na(combine)] <- 1e-7
+
+# dim check
+dim(C19vj)
+dim(vj)
+dim(patients)
+dim(combine)
