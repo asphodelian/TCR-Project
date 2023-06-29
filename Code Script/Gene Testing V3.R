a8df4933fadc393dd5907368a34fc5999835fe95
Gabrielle Salamanca <knightcallie73@hotmail.com>
2023-06-29 16:37:40 -0700

    Read in all three previous and fullgenes (gene) datasets; should have only read in the gene dataset

--- Code Script/Gene Testing V3.R
+++ Code Script/Gene Testing V3.R
@@ -10,3 +10,14 @@ library(readxl)
 library(SKAT)
 library(tidyr)
 library(xlsx)
+
+############
+# Datasets #
+############
+
+C19vj <- read_csv("D:/Coding/R Storage/Summer TCR Project/TCR Datasets/dt.COVID_TCR.vjGene.p.csv",
+                  show_col_types = FALSE)
+vj <- read_csv("D:/Coding/R Storage/Summer TCR Project/TCR Datasets/dt.HD_TCR.vjGene.p.csv",
+               show_col_types = FALSE)
+patients <- read_excel("D:/Coding/R Storage/Summer TCR Project/TCR Datasets/dt.info_edited.xlsx")
+gene <- read_excel("fullgenes.xlsx")
