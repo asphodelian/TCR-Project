88d97b5edd900e886efad8eae917cfaf7f743712
Gabrielle Salamanca <knightcallie73@hotmail.com>
2023-06-29 16:38:59 -0700

    describe() only the first three

--- Code Script/Gene Testing V3.R
+++ Code Script/Gene Testing V3.R
@@ -42,3 +42,11 @@ patients <- patients %>%
             as.factor)
 cat("Summary of patients as factor: \n\n")
 summary(patients)
+
+############
+# Describe #
+############
+
+describe(C19vj)
+describe(vj)
+describe(patients)
