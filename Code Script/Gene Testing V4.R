4bef84c2197eaa4fd7c438fa62d639cc2974e38e
Gabrielle Salamanca <knightcallie73@hotmail.com>
2023-06-29 17:18:27 -0700

    Beginning of SKAT attempt, note from 6/26 meeting

--- Code Script/Gene Testing V4.R
+++ Code Script/Gene Testing V4.R
@@ -15,3 +15,13 @@ library(tidyr)
 ###########
 
 gene <- read_excel("fullgenes.xlsx")
+
+################
+# SKAT Attempt #
+################
+
+# Note 6/26:
+# Read M448 Book Ch12 
+# pca plots, color dots with Y, do with all 710 genes & significant v genes 
+# p.adjust(p, method = p.adjust.methods, n = length(p)) for all p-values
+# Try the SKATBin with the j gene, vjgene
