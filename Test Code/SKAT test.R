438309dddaf477fc411838dfa463f6fbd9304f30
Gabrielle Salamanca <knightcallie73@hotmail.com>
2023-07-03 12:38:14 -0700

    assocation test for cont trait

--- Test Code/SKAT test.R
+++ Test Code/SKAT test.R
@@ -6,3 +6,11 @@ library(SKAT)
 data(SKAT.example)
 names(SKAT.example)
 attach(SKAT.example)
+
+# cont trait
+obj1 <- SKAT_Null_Model(y.c ~ X, out_type = "C") # Y ~ V10 dataset
+out1.c <- SKAT(Z, obj1)
+out1.c$p.value
+
+objv10.1 <- SKAT_Null_Model(genes$Y ~ 1, out_type = "D")
+outv10.1 <- SKAT(genes[,1:13],objv10.1)
