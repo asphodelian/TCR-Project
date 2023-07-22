c63a20f3dc4f059c3c85c5a041f1770254276aad
Gabrielle Salamanca <knightcallie73@hotmail.com>
2023-07-22 14:32:17 -0700

    New script to try a new parameter, added in libraries

---
+++ Code Script/Gene Testing V6.R
@@ -0,0 +1,14 @@
+###########
+# Library #
+###########
+
+library(dplyr)
+library(factoextra)
+library(ggfortify)
+library(ggplot2)
+library(gridExtra)
+library(psych)
+library(readr)
+library(readxl)
+library(SKAT)
+library(tidyr)
