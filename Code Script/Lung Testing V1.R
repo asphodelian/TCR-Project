fc4442e66fb04d6728066ad6e24cec21102ed62e
Gabrielle Salamanca <knightcallie73@hotmail.com>
2023-07-31 15:47:48 -0700

    New script for lung datasets, added necessary libraries

---
+++ Code Script/Lung Testing V1.R
@@ -0,0 +1,20 @@
+###########
+# Library #
+###########
+
+library(dplyr)
+library(factoextra)
+library(ggfortify)
+library(ggplot2)
+library(gridExtra)
+library(openxlsx)
+library(psych)
+library(readr)
+library(readxl)
+library(SKAT)
+library(tidyr)
+library(xlsx)
+
+
+
+
