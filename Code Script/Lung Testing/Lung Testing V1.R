cca119246fbbeed3b8b91cdf528c2e66534292bb
Gabrielle Salamanca <knightcallie73@hotmail.com>
2023-07-31 18:03:38 -0700

    Added summaries of datasets

--- Code Script/Lung Testing/Lung Testing V1.R
+++ Code Script/Lung Testing/Lung Testing V1.R
@@ -24,6 +24,23 @@ lung <- read_csv("C:/Users/knigh/OneDrive/Desktop/Github/TCR-Project/Datasets/Lu
 lungenes <- read_csv("C:/Users/knigh/OneDrive/Desktop/Github/TCR-Project/Datasets/Lung Data/vjGene.p.all.bytime.csv",
                      show_col_types = FALSE)
 
+#############
+# Summaries #
+#############
+
+lung <- lung %>% 
+  mutate_at(c('Patient.Status', 'Phase', 'Disease.Type', 'TRTP', 'DISTYP', 
+              'PDL1_TC', 'PDL1STAT', 'AGEU', 'AGEGR1', 'SEX', 'RACE', 
+              'RACEGR1', 'SMOKHIST', 'SMOKGR1', 'COUNTRY', 'REGION1', 
+              'LINTXGR1', 'PRPLAT', 'PLATMTYP', 'Status', 'LIVERBL',
+              'Response', 'Response.bin1', 'Response.bin2'), as.factor)
+cat("Summary of lung as factors: \n\n")
+summary(lung)
+
+lungenes <- lungenes %>% 
+  mutate_at(c('...1'), as.factor)
+cat("Summary of lungenes as factors: \n\n")
+summary(lungenes)
 
 
 
