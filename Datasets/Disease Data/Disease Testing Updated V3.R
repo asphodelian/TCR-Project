f1bb877043114b59e181700fa15294b60942fdd3
Gabrielle Salamanca <knightcallie73@hotmail.com>
2023-08-10 10:59:35 -0700

    Prof Tao's version of data prep

--- Datasets/Disease Data/Disease Testing Updated V3.R
+++ Datasets/Disease Data/Disease Testing Updated V3.R
@@ -20,3 +20,28 @@ C19vj <- read_csv("D:/Coding/R Storage/Summer TCR Project/TCR Datasets/dt.COVID_
 vj <- read_csv("D:/Coding/R Storage/Summer TCR Project/TCR Datasets/dt.HD_TCR.vjGene.p.csv",
                show_col_types = FALSE)
 patients <- read_excel("D:/Coding/R Storage/Summer TCR Project/TCR Datasets/dt.info_edited.xlsx")
+
+#############
+# Data Prep #
+#############
+
+# combine the vj datasets
+combine <- merge(C19vj, vj, by = "vjGene")
+
+# replace NAs w/ extremely small number
+combine[is.na(combine)] <- 1e-7
+
+# rename rows as vjGene
+row.names(combine) <- combine[,1]
+
+# removed col
+combine$vjGene <- NULL
+
+# log
+combine <- log(combine) 
+
+# transpose
+transComb <- t(combine)
+
+# standardize
+transComb.std <- scale(transComb)
