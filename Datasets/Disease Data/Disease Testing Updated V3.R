5f1cda06f51f336bf3d1307e4af020d1462021bf
Gabrielle Salamanca <knightcallie73@hotmail.com>
2023-08-10 10:39:46 -0700

    Updated V3 made: added libraries and datasets

---
+++ Datasets/Disease Data/Disease Testing Updated V3.R
@@ -0,0 +1,22 @@
+###########
+# Library #
+###########
+
+library(dplyr)
+library(openxlsx)
+library(psych)
+library(readr)
+library(readxl)
+library(SKAT)
+library(tidyr)
+library(xlsx)
+
+############
+# Datasets #
+############
+
+C19vj <- read_csv("D:/Coding/R Storage/Summer TCR Project/TCR Datasets/dt.COVID_TCR.vjGene.p.csv",
+                  show_col_types = FALSE)
+vj <- read_csv("D:/Coding/R Storage/Summer TCR Project/TCR Datasets/dt.HD_TCR.vjGene.p.csv",
+               show_col_types = FALSE)
+patients <- read_excel("D:/Coding/R Storage/Summer TCR Project/TCR Datasets/dt.info_edited.xlsx")
