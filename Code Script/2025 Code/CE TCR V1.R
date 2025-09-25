756a93fa6279718c6d54e83374443c389eba1346
Gabrielle Salamanca <knightcallie73@hotmail.com>
2025-09-25 00:11:46 -0700

    Read in dataset and there are missing values to address

--- Code Script/2025 Code/CE TCR V1.R
+++ Code Script/2025 Code/CE TCR V1.R
@@ -23,3 +23,18 @@ library(MASS) # QDA & LDA
 library(class) # KNN
 library(leaps) # best subset
 library(glmnet) # ridge regress & LASSO
+
+###########
+# Dataset #
+###########
+
+gene <- read_excel("C:/Users/knigh/OneDrive/Desktop/Github/TCR-Project/Datasets/Disease Data/fullgenes.xlsx")
+attach(gene)
+
+dim(gene)
+head(names(gene))
+summary(gene)
+
+View(gene)
+
+# missing values, need to address
