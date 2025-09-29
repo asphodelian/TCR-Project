15a4c561ee2dc086c7deec07366fe821ed6e83d7
Gabrielle Salamanca <knightcallie73@hotmail.com>
2025-09-29 16:19:00 -0700

    Meeting with Tao, discussed about dataset and how to proceed forward with choosing best vars for fits

--- Code Script/2025 Code/CE TCR V1.R
+++ Code Script/2025 Code/CE TCR V1.R
@@ -30,19 +30,19 @@ library(glmnet) # ridge regress & LASSO
 
 # having onedrive issues, so reading from personal storage
 gene <- read_excel("D:/Coding/R Storage/Summer TCR Project/TCR Datasets/2025/fullgenes.xlsx")
-attach(gene)
+#attach(gene)
 
 dim(gene)
 head(names(gene))
 summary(gene)
 
-View(gene)
+#View(gene)
 
 # missing values in dataset
 rows <- c(22,94:110)
 genedit <- gene[-rows,]
 dim(genedit)
-View(genedit)
+#View(genedit)
 
 #training & test data
 train <- sample(1:nrow(genedit),0.8*nrow(genedit))
@@ -53,11 +53,24 @@ test.data <- genedit[-train,]
 # dependent var
 y <- train.data$Y # binary
 y[y == "disease"] <- 1 
+y[y == "healthy"] <- 0
+train.data$Y <- as.numeric(y)
 
 table(train.data$Y)
 
 
-
+gene.name <- names(train.data)[2:ncol(train.data)-2]
+pvalue <- rep(0,length(gene.name))
 #glm.fit
-glm.fit <- glm(y ~ ., train.data, family = "binomial")
+for (i in 1:length(gene.name))
+{
+ # i <- 2
+  data <- train.data[,c(i+1,ncol(train.data)-1)]
+  glm.fit <- glm(Y~.,data,family="binomial")
+  result <- summary(glm.fit)
+  print(i)
+  print(result$coefficients[2,4])
+  
+}
+glm.fit <- glm(Y ~train.data[,2], train.data, family = "binomial")
 summary(glm.fit)
