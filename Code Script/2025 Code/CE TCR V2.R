d3ba9b688fcab2966611d47f8a646d094d42ab38
Gabrielle Salamanca <knightcallie73@hotmail.com>
2025-10-05 00:55:43 -0700

    read in dataset and double checked the NAs

--- Code Script/2025 Code/CE TCR V2.R
+++ Code Script/2025 Code/CE TCR V2.R
@@ -23,3 +23,13 @@ library(MASS) # QDA & LDA
 library(class) # KNN
 library(leaps) # best subset
 library(glmnet) # ridge regress & LASSO
+
+###########
+# Dataset #
+###########
+
+gene <- read_excel("D:/Coding/R Storage/Summer TCR Project/TCR Datasets/2025/fullgenes.xlsx")
+dim(gene)
+head(names(gene))
+table(is.na(gene$Y))
+table(is.na(gene$Y1))
