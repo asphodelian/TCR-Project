0738a39d2a6abf5b7eb32fb08f48f680d1686c4a
Gabrielle Salamanca <knightcallie73@hotmail.com>
2025-10-06 18:00:37 -0700

    KNN problems, prof said GROUP the best 5

--- Code Script/2025 Code/CE TCR V2.R
+++ Code Script/2025 Code/CE TCR V2.R
@@ -47,6 +47,8 @@ table(is.na(genedit$Y1))
 # Train/Test #
 ##############
 
+set.seed(895)
+
 train <- sample(1:nrow(genedit),0.8*nrow(genedit))
 test <- -train
 train.data <- genedit[train,]
@@ -92,20 +94,26 @@ head(results)
 ##############
 # GLM: Top 5 #
 ##############
+
+# group first 5 in glm
+
 glm.fit1 <- glm(Y ~ `TRBV9_TRBJ2-7`, data = train.data, family = binomial())
 summary(glm.fit1) # both signif
 
 glm.fit2 <- glm(Y ~ `TRBV23-1_TRBJ2-2`, data = train.data, family = binomial())
 summary(glm.fit2) # both signif
 
-glm.fit3 <- glm(Y ~ `TRBV7-6_TRBJ2-6`, data = train.data, family = binomial())
+glm.fit3 <- glm(Y ~ `TRBV2_TRBJ1-6`, data = train.data, family = binomial())
 summary(glm.fit3)
 
-glm.fit4 <- glm(Y ~ `TRBV4-1_TRBJ2-3`, data = train.data, family = binomial())
+glm.fit4 <- glm(Y ~ `TRBV21-1_TRBJ2-3`, data = train.data, family = binomial())
 summary(glm.fit4) # both signif
 
-glm.fit5 <- glm(Y ~ `TRBV2_TRBJ1-6`, data = train.data, family = binomial())
+glm.fit5 <- glm(Y ~ `TRBV12-5_TRBJ2-6`, data = train.data, family = binomial())
 summary(glm.fit5)
+
+glm.fit6 <- glm(Y ~ `TRBV9_TRBJ2-7` + `TRBV23-1_TRBJ2-2` + `TRBV2_TRBJ1-6` + `TRBV21-1_TRBJ2-3` + `TRBV12-5_TRBJ2-6`, data = train.data, family = binomial())
+summary(glm.fit6)
 
 ###################################
 # Quadratic Discriminant Analysis #
@@ -114,6 +122,12 @@ summary(glm.fit5)
 test.data$Y[test.data$Y == "disease"] <- 1 
 test.data$Y[test.data$Y == "healthy"] <- 0
 test.data$Y <- as.numeric(test.data$Y)
+
+qda.fit6 <- qda(Y ~ `TRBV9_TRBJ2-7` + `TRBV23-1_TRBJ2-2` + `TRBV2_TRBJ1-6` + `TRBV21-1_TRBJ2-3` + `TRBV12-5_TRBJ2-6`, data = train.data)
+qda.fit6
+qda.class6 <- predict(qda.fit6, test.data)$class
+table(qda.class6, test.data$Y)
+mean(qda.class6 == test.data$Y)
 
 qda.fit1 <- qda(Y ~ `TRBV9_TRBJ2-7`, data = train.data)
 qda.fit1
@@ -154,6 +168,13 @@ mean(qda.class5 == test.data$Y)
 # Linear Discriminant Analysis #
 ################################
 
+lda.fit6 = lda(Y ~ `TRBV9_TRBJ2-7` + `TRBV23-1_TRBJ2-2` + `TRBV2_TRBJ1-6` + `TRBV21-1_TRBJ2-3` + `TRBV12-5_TRBJ2-6`, data = train.data) 
+lda.fit6
+plot(lda.fit1) # margins too large
+lda.class6 = predict(lda.fit6, test.data)$class
+table(lda.class6, test.data$Y)
+mean(lda.class6 == test.data$Y)
+
 lda.fit1 = lda(Y ~ `TRBV9_TRBJ2-7`, data = train.data) 
 lda.fit1
 #plot(lda.fit1) # margins too large
@@ -189,3 +210,20 @@ lda.class5 = predict(lda.fit5, test.data)$class
 table(lda.class5, test.data$Y)
 mean(lda.class5 == test.data$Y)
 # 0.7727273
+
+########################
+# K-Nearest Neighbors ##
+########################
+
+noY1 <- genedit[,-679]
+
+train <- sample(1:nrow(noY1),0.8*nrow(noY1))
+test <- -train
+
+train.X = genedit[train,]
+test.X = genedit[-train,]
+y.train = train.data$Y
+
+knn.pred = knn(train.data, test.data, y.train, k = 1)
+table(knn.pred, test.data$Y)
+mean(knn.pred == test.data$Y)
