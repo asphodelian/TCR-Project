4eda7a0b9ec1c79575626f9962b09d9ce183a499
Gabrielle Salamanca <knightcallie73@hotmail.com>
2025-10-12 18:55:05 -0700

    New code script, added libraries

---
+++ Code Script/2025 Code/CE TCR V3.R
@@ -0,0 +1,25 @@
+##########################
+# Culminating Experience #
+##########################
+
+#############
+# Libraries #
+#############
+
+library(dplyr)
+library(factoextra)
+library(ggfortify)
+library(ggplot2)
+library(gridExtra)
+library(psych)
+library(readr)
+library(readxl)
+library(SKAT)
+library(tidyr)
+
+library(caret)
+library(stats)
+library(MASS) # QDA & LDA
+library(class) # KNN
+library(leaps) # best subset
+library(glmnet) # ridge regress & LASSO
