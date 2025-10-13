45cca01e095d388fc0b92e8ddb20a1dea78f224c
Gabrielle Salamanca <knightcallie73@hotmail.com>
2025-10-12 18:55:33 -0700

    Added dataset

--- Code Script/2025 Code/CE TCR V3.R
+++ Code Script/2025 Code/CE TCR V3.R
@@ -21,5 +21,22 @@ library(caret)
 library(stats)
 library(MASS) # QDA & LDA
 library(class) # KNN
-library(leaps) # best subset
-library(glmnet) # ridge regress & LASSO
+
+###########
+# Dataset #
+###########
+
+gene <- read_excel("D:/Coding/R Storage/Summer TCR Project/TCR Datasets/2025/fullgenes.xlsx")
+dim(gene)
+head(names(gene))
+table(is.na(gene$Y))
+table(is.na(gene$Y1))
+
+is.na(gene$Y[22])
+is.na(gene$Y1[22])
+genedit <- gene[-22,]
+dim(genedit)
+
+genedit[is.na(genedit)] <- "healthy" 
+table(is.na(genedit$Y))
+table(is.na(genedit$Y1))
