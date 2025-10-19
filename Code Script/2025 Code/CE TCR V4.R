a607b907af47164e2c2c658878e27d3b31708e03
Gabrielle Salamanca <knightcallie73@hotmail.com>
2025-10-18 19:42:59 -0700

    Read in dataset

--- Code Script/2025 Code/CE TCR V4.R
+++ Code Script/2025 Code/CE TCR V4.R
@@ -21,3 +21,22 @@ library(caret)
 library(stats)
 library(MASS) # QDA & LDA
 library(class) # KNN 
+
+###########
+# Dataset #
+###########
+
+gene <- read_excel("D:/Coding/R Storage/Summer TCR Project/TCR Datasets/2025/fullgenes.xlsx")
+dim(gene)
+head(names(gene))
+table(is.na(gene$Y))
+table(is.na(gene$Y1))
+
+is.na(gene$Y[22])
+is.na(gene$Y1[22])
+genedit <- gene[-22,]
+dim(genedit)
+
+genedit[is.na(genedit)] <- "healthy" 
+table(is.na(genedit$Y))
+table(is.na(genedit$Y1))
