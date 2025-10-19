fbe51a8357a8a4454a0d58171ef71e366f0cdbda
Gabrielle Salamanca <knightcallie73@hotmail.com>
2025-10-18 19:43:26 -0700

    Split into training and test

--- Code Script/2025 Code/CE TCR V4.R
+++ Code Script/2025 Code/CE TCR V4.R
@@ -40,3 +40,24 @@ dim(genedit)
 genedit[is.na(genedit)] <- "healthy" 
 table(is.na(genedit$Y))
 table(is.na(genedit$Y1))
+
+##############
+# Train/Test #
+##############
+
+set.seed(895)
+
+train <- sample(1:nrow(genedit),0.8*nrow(genedit))
+test <- -train
+train.data <- genedit[train,]
+test.data <- genedit[-train,]
+
+# turn Y binary
+y <- train.data$Y 
+y[y == "disease"] <- 1 
+y[y == "healthy"] <- 0
+train.data$Y <- as.numeric(y)
+
+test.data$Y[test.data$Y == "disease"] <- 1
+test.data$Y[test.data$Y == "healthy"] <- 0
+test.data$Y <- as.numeric(test.data$Y)
