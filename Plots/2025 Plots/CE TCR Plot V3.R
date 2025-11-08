3ae08ad2b4981881d33217a31468d63d6a5a54ba
Gabrielle Salamanca <knightcallie73@hotmail.com>
2025-11-07 22:05:34 -0800

    updated libraries

--- Plots/2025 Plots/CE TCR Plot V3.R
+++ Plots/2025 Plots/CE TCR Plot V3.R
@@ -6,23 +6,29 @@
 # Libraries #
 #############
 
-library(corrplot)
+# data manipulation
 library(dplyr)
+library(psych)
+library(readr)
+library(readxl)
+library(tidyr)
+
+# visualize
 library(factoextra)
 library(ggfortify)
 library(ggplot2)
 library(gridExtra)
-library(psych)
-library(readr)
-library(readxl)
-library(scales)
-library(SKAT)
-library(tidyr)
 
-library(caret)
-library(stats)
+# techniques
+library(brglm2) # bias-reduced log regress
+library(caret) # data partition, CV
+library(class) # KNN 
+library(gbm) # boosting
 library(MASS) # QDA & LDA
-library(class) # KNN
+library(randomForest) # bag, rand forest
+library(rpart) # class tree
+library(stats)
+library(tree) # regression tree
 
 ###########
 # Dataset #
