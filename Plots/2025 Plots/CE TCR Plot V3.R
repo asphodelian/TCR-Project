2953ecdb7f9a0451731a361f5464bfbb44a99e61
Gabrielle Salamanca <knightcallie73@hotmail.com>
2025-11-07 20:28:15 -0800

    New TCR plot for advanced techniques, added libraries and dataset read

---
+++ Plots/2025 Plots/CE TCR Plot V3.R
@@ -0,0 +1,43 @@
+##########################
+# Culminating Experience #
+##########################
+
+#############
+# Libraries #
+#############
+
+library(corrplot)
+library(dplyr)
+library(factoextra)
+library(ggfortify)
+library(ggplot2)
+library(gridExtra)
+library(psych)
+library(readr)
+library(readxl)
+library(SKAT)
+library(tidyr)
+
+library(caret)
+library(stats)
+library(MASS) # QDA & LDA
+library(class) # KNN
+
+###########
+# Dataset #
+###########
+
+gene <- read_excel("D:/Coding/R Storage/Summer TCR Project/TCR Datasets/2025/fullgenes.xlsx")
+dim(gene)
+head(names(gene))
+table(is.na(gene$Y))
+table(is.na(gene$Y1))
+
+is.na(gene$Y[22])
+is.na(gene$Y1[22])
+genedit <- gene[-22,]
+dim(genedit)
+
+genedit[is.na(genedit)] <- "healthy" 
+table(is.na(genedit$Y))
+table(is.na(genedit$Y1))
