ec4bcf304f56db9c24b73d78f9b9239bb18bf9fe
Gabrielle Salamanca <knightcallie73@hotmail.com>
2025-11-11 01:32:05 -0800

    Added dataset

--- Plots/2025 Plots/CE TCR Plot V4.R
+++ Plots/2025 Plots/CE TCR Plot V4.R
@@ -31,3 +31,21 @@ library(rpart) # class tree
 library(stats)
 library(tree) # regression tree
 
+###########
+# Dataset #
+###########
+
+gene <- read_excel("D:/Coding/R Storage/Summer TCR Project/TCR Datasets/2025/fullgenes.xlsx")
+dim(gene)
+head(names(gene))
+table(is.na(gene$Y))
+table(is.na(gene$Y1))
+
+is.na(gene$Y[22])
+is.na(gene$Y1[22])
+genedit <- gene[-22,]
+dim(genedit)
+
+genedit[is.na(genedit)] <- "healthy" 
+table(is.na(genedit$Y))
+table(is.na(genedit$Y1))
