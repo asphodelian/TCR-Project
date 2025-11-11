35513ed4ca221ea205b2d97f202a18a9139ff8be
Gabrielle Salamanca <knightcallie73@hotmail.com>
2025-11-11 01:31:03 -0800

    New plot script to combine all techniques, added libraries

---
+++ Plots/2025 Plots/CE TCR Plot V4.R
@@ -0,0 +1,33 @@
+##########################
+# Culminating Experience #
+##########################
+
+#############
+# Libraries #
+#############
+
+# data manipulation
+library(dplyr)
+library(psych)
+library(readr)
+library(readxl)
+library(tidyr)
+
+# visualize
+library(factoextra)
+library(ggfortify)
+library(ggplot2)
+library(gridExtra)
+library(scales)
+
+# techniques
+library(brglm2) # bias-reduced log regress
+library(caret) # data partition, CV
+library(class) # KNN 
+library(gbm) # boosting
+library(MASS) # QDA & LDA
+library(randomForest) # bag, rand forest
+library(rpart) # class tree
+library(stats)
+library(tree) # regression tree
+
