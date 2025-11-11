2aab2e698f9adbf436e31b734255d665fe1c0a21
Gabrielle Salamanca <knightcallie73@hotmail.com>
2025-11-11 01:33:08 -0800

    Created significant genes dataset

--- Plots/2025 Plots/CE TCR Plot V4.R
+++ Plots/2025 Plots/CE TCR Plot V4.R
@@ -64,3 +64,37 @@ test.data <- genedit[-train,]
 # turn Y binary
 train.data$Y <- as.numeric(ifelse(train.data$Y == "disease", 1, 0))
 test.data$Y  <- as.numeric(ifelse(test.data$Y  == "disease", 1, 0))
+
+#####################
+# Significant Genes #
+#####################
+
+col <- ncol(train.data)
+ycol <- match("Y", names(train.data))
+gene_idx  <- 2:(col - 2)
+gene.name <- names(train.data)[gene_idx]
+pvalue <- numeric(length(gene_idx))
+
+for (i in seq_along(gene_idx))
+{
+  gene_name <- gene.name[i]
+  Xi <- train.data[, gene_idx[i], drop = FALSE]
+  names(Xi) <- gene_name  # set column name to the gene
+  
+  dat <- data.frame(Y = train.data[[ycol]], Xi, check.names = FALSE)
+  glm.fit <- glm(Y ~ ., data = dat, family = binomial())
+  pvalue[i] <- coef(summary(glm.fit))[2, 4]
+}
+
+# Combine results into a nice table:
+results <- data.frame(Gene = gene.name, P_value = pvalue)
+
+# Sort by significance
+results <- results[order(results$P_value), ]
+head(results)
+
+alpha <- results[results$P_value < 0.05,]
+dim(alpha)
+
+ranked <- alpha$Gene
+ranked <- intersect(ranked, intersect(names(train.data), names(test.data)))
